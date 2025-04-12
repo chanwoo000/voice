bdcd4fc74ddc26feb6c267d00577ec9af0573a56
chanwoo <chanwoo0607@naver.com>
2025-04-12 14:51:25 +0900

    test9

--- frontend/src/hooks/useVoicepackConvert.js
+++ frontend/src/hooks/useVoicepackConvert.js
@@ -16,14 +16,7 @@ const useVoiceConvert = () => {
     formData.append('name', voicePackName);
     formData.append('voiceFile', new File([audioBlob], 'voice.wav', {type: 'audio/wav'}));
 
-    console.log('✅ userId:', userId); // 현재는 하드코딩 7 → OK
-    console.log('✅ name:', voicePackName); // 비어 있으면 ❌
-    console.log('✅ audioBlob:', audioBlob); // size가 0이면 ❌
-    console.log('✅ voiceFile File 객체:', new File([audioBlob], 'voice.wav', {type: 'audio/wav'}));
-
     const url = 'voicepack/convert';
-    console.log('🌐 전송할 API URL:', axiosInstance.defaults.baseURL + url);
-
     try {
       setLoading(true);
       setError(null);
@@ -34,8 +27,8 @@ const useVoiceConvert = () => {
         },
         withCredentials: true,
       });
-
       return response.data;
+
     } catch (err) {
       console.error('보이스팩 변환 오류:', err);
       setError(err);
