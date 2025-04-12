52d2c4deed9b1e9af0273d6f1ffa5e1fed6da01d
chanwoo <chanwoo0607@naver.com>
2025-04-12 14:42:51 +0900

    test8

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -4,9 +4,6 @@ const axiosInstance = axios.create({
 
   baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true, // 쿠키 전송 허용
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 export default axiosInstance;
